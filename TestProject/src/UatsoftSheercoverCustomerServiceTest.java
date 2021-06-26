91c7b863366626d325179ec94f2d7cdc1bae8569
Naren90701 <naren90701@hotmail.com>
2021-06-25 20:28:56 -0700

    This test verifies Sheercover Customer service feature Test

---
+++ TestProject/src/UatsoftSheercoverCustomerServiceTest.java
@@ -0,0 +1,11 @@
+
+public class UatsoftSheercoverCustomerServiceTest {
+	
+	
+public static void main(String [] args) {
+	
+	
+	System.out.println(" This code will test the Sheercover Customer Service Feature");
+}
+	
+}
